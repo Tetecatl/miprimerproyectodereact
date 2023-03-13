b81823a3ba1728d8ac0493e64734a67342611daf
aliciaflores <aflorest98@gmail.com>
2023-03-13 13:27:46 -0600

    01-const-let

---
+++ bases/01-const-let.js
@@ -0,0 +1,14 @@
+//variables y constantes
+const nombre = 'Alicia';
+const apellidos= 'Flores';
+let valorDado = 24;
+valorDado=20;
+
+console.log(nombre, apellidos, valorDado);
+
+if(true){
+    const nombre='Anahi';
+    console.log(nombre);
+}
+
+console.log(valorDado);
